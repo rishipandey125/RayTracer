e2e69476e4c6970f6513e47f7a4659ff2c183a74
rishipandey125 <rpandey2@illinois.edu>
2020-07-27 18:04:56 -0500

    overloaded opreators

--- vec3.h
+++ vec3.h
@@ -6,11 +6,12 @@ class vec3 {
     vec3();
     vec3(float i, float j, float k);
     float length();
+    void unit();
     vec3 operator+(const vec3 &vector);
     vec3 operator-(const vec3 &vector);
     vec3 operator*(const vec3 &vector);
 
-  private:
+  public:
     float x;
     float y;
     float z;
