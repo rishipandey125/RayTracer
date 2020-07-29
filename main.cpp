9941cbd471cea14b82c2eeec72dc688c465aadb8
rishipandey125 <rpandey2@illinois.edu>
2020-07-29 10:13:26 -0500

    added constructor to sphere.h

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include "vec.cpp"
 #include "ray.cpp"
-#include "sphere.cpp
+#include "sphere.cpp"
+
+void output_color(const color &pixel) {
+  int r = static_cast<int>(255.999 * pixel.x);
+  int g = static_cast<int>(255.999 * pixel.y);
+  int b = static_cast<int>(255.999 * pixel.z);
+  std::cout << r << ' ' << g << ' ' << b << '\n';
+}
 
 bool hit_sphere(const ray &casted_ray, const sphere &object) {
   vec ac = casted_ray.origin - object.center;
@@ -11,11 +18,12 @@ bool hit_sphere(const ray &casted_ray, const sphere &object) {
   float discriminant = (b*b) - (4*a*c);
   return (discriminant > 0);
 }
-//raytracing hit sphere makes sense, but is it disc>0 or disc>=0
+
 int main() {
-  float aspect_ratio = 16.0/9.0;
   //Image Details
-  sphere first_sphere()
+  float aspect_ratio = 16.0/9.0;
+  point sphere_center(0,0,-1);
+  sphere first_sphere(sphere_center,0.5);
   int image_width = 400;
   int image_height = (int)(image_width/aspect_ratio);
 
@@ -24,6 +32,7 @@ int main() {
   float viewport_height = 2.0;
   float viewport_width = viewport_height*aspect_ratio;
   float focal_length = 1.0; // sets viewport at z = -1
+
   //Render Details
   // set viewport rays to cast through image loop
   // std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -34,16 +43,13 @@ int main() {
           point viewport_point((u*viewport_width)-(viewport_width/2),(v*viewport_height)-(viewport_height/2),-1*focal_length);
           vec direction = viewport_point-camera_origin;
           ray cast_ray(camera_origin,direction);
-
-          //
-          // auto r = double(i) / (image_width-1);
-          // auto g = double(j) / (image_height-1);
-          // auto b = 0.25;
-          // color pixel = vec(r,g,b);
-          // int ir = static_cast<int>(255.999 * pixel.x);
-          // int ig = static_cast<int>(255.999 * pixel.y);
-          // int ib = static_cast<int>(255.999 * pixel.z);
-          // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+          if (hit_sphere(cast_ray,first_sphere)) {
+            std::cout << "Hit Sphere" << std::endl;
+            //color red
+          } else {
+            continue;
+            //color white
+          }
       }
   }
   return 0;
--- sphere.h
+++ sphere.h
@@ -4,6 +4,7 @@
 class sphere {
   public:
     sphere();
+    sphere(point sphere_center,float sphere_rad);
     point center;
     float radius;
     vec get_normal_vector();
