c672a40d1cbd6a4a0119eb67d93e30975e2b9912
rishipandey125 <rpandey2@illinois.edu>
2020-08-04 11:14:13 -0500

    distortion is normal
    once i include camera features such as fov it will fix distortion on items

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ int main() {
   camera cam;
   //my axis system is not actually working well, and i am getting a distortion on my sphere geometry
   // sphere world_sphere(point(0,-50.5,-1),50);
-  sphere first_sphere(point(0.5,-.5,-1),0.5);
+  sphere first_sphere(point(0,0,-1),0.5);
   std::vector <sphere> spheres = {first_sphere};
   int image_width = 1000;
   int image_height = (int)(image_width/cam.aspect_ratio);
