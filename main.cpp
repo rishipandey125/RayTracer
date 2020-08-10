5333876ffa51b3d40fe3befb39c1c28d8f80d308
rishipandey125 <rpandey2@illinois.edu>
2020-08-10 17:33:31 -0500

    working on creating a hit structure

--- main.cpp
+++ main.cpp
@@ -21,6 +21,23 @@ if that hits something you keep diluting the end color (global illumination)
 Keep doing this until you stop hitting stuff or you reach the ray depth
 We are using global illumination (so there is no local light source the sky is essentially the light source)
 */
+
+struct hit {
+  bool success;
+  float t;
+  sphere object;
+  hit() {
+    bool = false;
+    t = 0.0;
+    object = NULL;
+  }
+  hit(bool s, float val, sphere sp) {
+    success = s;
+    t = s;
+    object = sp;
+  }
+}
+
 float random_float() {
   return ((float) rand()/RAND_MAX);
 }
@@ -36,23 +53,32 @@ vec random_unit_vector() {
   return vec(r*cos(a),r*sin(a),z);
 }
 
-color trace(ray &casted_ray, std::vector <sphere> &objects) {
+color trace(ray &casted_ray, std::vector <sphere> &objects, int depth) {
   //create gradient background
+  if (depth <= 0) {
+    return color(0,0,0);
+  }
   vec unit_direction = casted_ray.direction;
   unit_direction.unit();
   float val = (unit_direction.y+1.0)/2.0;
   color pixel = color(1.0, 1.0, 1.0)*(1-val) + color(0.5, 0.7, 1.0)*val;
   float closest = float(RAND_MAX); //closest t (whatever is closest to camera is what you render)
+  //save which object is hit basically by return the sphere, point, and normal vec?
+  //create a separate hit func? or hit structure
+
+  //create a hit object (after iterating through all objects we have the nearest hit if there is one)
   for (int i = 0; i < objects.size(); i++) {
     float t = objects[i].hit_sphere(casted_ray);
     if (t > 0.0) {
-      //hit
       if (t < closest) {
+
         closest = t;
         pixel = color(1,0,0); //this is where we would do a recursive call
       }
     }
   }
+  // if () we hit something 
+  //if you did hit something, retrace from that point and normal vec
   return pixel;
 }
 
@@ -84,7 +110,7 @@ int main() {
             float u = (float(i) + random_float())/image_width;
             float v = (float(j) + random_float())/image_height;
             ray cast_ray = cam.get_ray(u,v);
-            pixel = pixel + trace(cast_ray,spheres);
+            pixel = pixel + trace(cast_ray,spheres,50);
           }
         output_color(pixel,samples);
       }
