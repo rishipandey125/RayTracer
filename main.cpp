33bbf81464e7ef8a44c9a66aefd2804bbdb044e4
rishipandey125 <rpandey2@illinois.edu>
2020-09-02 08:58:12 -0500

    building a video render system

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
   point camera_origin(0,0.8,0.1);
   point look_at(0,0,-1);
   float aspect_ratio = 16.0/9.0;
-  float vertical_fov = 60;
+  float vertical_fov = 20;
   float aperture = 0;
   camera cam(camera_origin,look_at, aspect_ratio,vertical_fov,aperture);
 
--- run_raytracer.sh
+++ run_raytracer.sh
@@ -1,3 +1,6 @@
 g++ -std=c++11 main.cpp
+#get the files to save to a folder
+#use ffmpeg to write it to a video given a 24fps framerate
+#delete the imgs and leave just the video
 ./a.out > test.ppm
 open test.ppm
