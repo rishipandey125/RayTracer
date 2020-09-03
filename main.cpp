caedf3310424b27f3450c2057c5a105ad3b3e6ab
rishipandey125 <rpandey2@illinois.edu>
2020-09-03 00:15:56 -0500

    render is going all over the place for fov i think it is a py issue

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ int main() {
   float aspect_ratio = 16.0/9.0;
   float start_fov = 90;
   float aperture = 0;
-  camera cam(camera_origin,look_at, aspect_ratio,start_fov,aperture);
 
   std::string folder_name = "/Users/rishipandey125/Desktop/RayTracedContent/";
 
-  int frame_num = 1;
+  int frame_num = 0;
   float end_fov = 60;
   for (int x = 0; x < 72; x++) {
     std::ostringstream num;
     frame_num += 1;
     num<<frame_num;
     std::string frame_name = folder_name+"frame"+num .str()+".ppm";
-    cam.vertical_fov = start_fov - ((start_fov-end_fov)*(x/72));
+    float vfov = start_fov - ((start_fov-end_fov)*(float(x)/72.0));
+    camera cam(camera_origin,look_at, aspect_ratio,vfov,aperture);
     render_frame(cam,frame_name);
   }
   return 0;
--- stitch_frames.py
+++ stitch_frames.py
@@ -3,7 +3,8 @@ import cv2
 images_path = "/Users/rishipandey125/Desktop/RayTracedContent/"
 
 directory = os.fsencode(images_path)
-height = 0 width = 0
+height = 0
+width = 0
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".ppm"):
@@ -11,7 +12,7 @@ for file in os.listdir(directory):
         height, width, layers = img.shape;
         break
 
-video = cv2.VideoWriter(images_path+"video.avi", 0, 1, (width,height))
+video = cv2.VideoWriter(images_path+"video.avi", 0, 24, (width,height))
 
 for file in os.listdir(directory):
      filename = os.fsdecode(file)
