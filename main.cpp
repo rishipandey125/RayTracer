9ea4771541ba9c41b30f2428d8d50c9590ce6755
rishipandey125 <rpandey2@illinois.edu>
2020-09-02 23:06:31 -0500

    going to try to use cv2 to save out as pngs and then stitch

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include "render.cpp"
-
+#include <sstream>
 
 int main() {
   point camera_origin(0,0.8,0.1);
   point look_at(0,0,-1);
   float aspect_ratio = 16.0/9.0;
-  float vertical_fov = 60;
+  float vertical_fov = 90;
   float aperture = 0;
   camera cam(camera_origin,look_at, aspect_ratio,vertical_fov,aperture);
-  render_frame(cam,"hello.ppm");
-  render_frame(cam,"hello2.ppm");
+  std::string folder_name = "/Users/rishipandey125/Desktop/RayTracedContent/";
+  float frame_num = 1;
+  std::ostringstream num;
+  num<<frame_num;
+  std::string frame_name = folder_name+"frame"+num .str()+".ppm";
+  render_frame(cam,frame_name);
   return 0;
 }
