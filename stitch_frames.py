ed6837807f0d6b06623642a19d96f8b5e64d6bdb
rishipandey125 <rpandey2@illinois.edu>
2020-09-03 13:11:52 -0500

    render pipeline working - trying 100 samples

--- stitch_frames.py
+++ stitch_frames.py
@@ -13,24 +13,25 @@ for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".ppm"):
         #set height and width for video
+        img = cv2.imread(images_path+filename)
         if height == 0 and width == 0:
-            img = cv2.imread(images_path+filename)
             height, width, layers = img.shape;
         #get which frame it is (frame_num -1 would be the index to place it at)
         frame_num = int(filename[:len(filename)-4])
         index = frame_num-1
         sorted_file_list[index] = filename
 
-video = cv2.VideoWriter(images_path+"video.avi", 0, 24, (width,height))
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+video = cv2.VideoWriter(images_path+"video.mp4", fourcc, 24, (width,height))
 
 
 
 for file in sorted_file_list:
-     filename = os.fsdecode(file)
-     if filename.endswith(".ppm"):
-         print(filename) #file names OUT OF ORDER
-         # img = cv2.imread(images_path+filename)
-         # video.write(img)
-         # os.remove(images_path+filename)
+    if file != None:
+        filename = os.fsdecode(file)
+    if filename.endswith(".ppm"):
+        img = cv2.imread(images_path+filename)
+        video.write(img)
+        os.remove(images_path+filename)
 
 print("Video Stitching Complete")
