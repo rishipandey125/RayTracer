d98b0f45ef1c822324e91e2cce64a1fa34154956
rishipandey125 <rpandey2@illinois.edu>
2020-09-08 10:34:54 -0500

    fov system works, testing apeture

--- main.cpp
+++ main.cpp
@@ -7,14 +7,17 @@ int main() {
   float aspect_ratio = 16.0/9.0;
   float start_fov = 90;
   float end_fov = 60;
-  float aperture = 0;
+  float apeture = 0;
   int total_frames = 72;
   camera cam(camera_origin,look_at,aspect_ratio,start_fov,end_fov,apeture,apeture,total_frames);
 
   std::string folder_name = "/Users/rishipandey125/Desktop/RayTracedContent/";
   while (!(cam.capture_complete)) {
+    cam.next_capture();
     std::ostringstream num;
     num<<cam.frame_count;
+    //frame count never updating
+    std::string frame_name = folder_name+num.str()+".ppm";
     render_frame(cam,frame_name);
   }
   return 0;
