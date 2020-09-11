3fae31a6bf379570ca10a3815b0f5e825d5449e0
rishipandey125 <rpandey2@illinois.edu>
2020-09-11 11:19:07 -0500

    working on final documentation

--- run_raytracer.sh
+++ run_raytracer.sh
@@ -1,6 +1,3 @@
-g++ -std=c++11 main.cpp
-./a.out
-python3 stitch_frames.py
-#get the files to save to a folder
-#use ffmpeg/cv2 to write it to a video given a 24fps framerate in python
-#delete the imgs and leave just the video
+g++ -std=c++11 main.cpp #compile raytracer
+./a.out #render frames
+python3 stitch_frames.py #stitch_frames into mp4
